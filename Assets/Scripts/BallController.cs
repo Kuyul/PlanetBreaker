2b18555b5e5b33ad93349687e8c3ab0906db9dcc
Kuyul <peyopeyo92@gmail.com>
2018-10-24 15:49:54 +1300

    Initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    //Declare Public variables
+    public float OrbitHeight1 = 2.5f;
+    public float FallStep = 0.1f;
+
+    //Declare Private variables
+    private float CurrentHeight = 0;
+    private bool Down = false;
+
+	// Use this for initialization
+	void Start () {
+        CurrentHeight = OrbitHeight1;
+        transform.localPosition = new Vector3(0, CurrentHeight, 0);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetMouseButton(0))
+        {
+            Down = true;
+        }
+
+        //User clicked, ball speeds down.
+        if (Down)
+        {
+            CurrentHeight -= FallStep;
+            transform.localPosition = new Vector3(0, CurrentHeight, 0);
+        }
+        else
+        {
+            if (CurrentHeight < OrbitHeight1)
+            {
+                CurrentHeight += FallStep;
+                if(CurrentHeight > OrbitHeight1)
+                {
+                    CurrentHeight = OrbitHeight1;
+                }
+                transform.localPosition = new Vector3(0, CurrentHeight, 0);
+            }
+        }
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //When planet is hit, put the ball back into orbit
+        if(collision.tag == "Planet")
+        {
+            Down = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Declare Public Variables
+    public float AngularVelocity = 30.0f;
+
+    //Declare Private Variables
+    private Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.angularVelocity = AngularVelocity;
+	}
+}
