de35b3efac5f2335682db4a39edd5505ba7a9151
Kuyul <peyopeyo92@gmail.com>
2018-10-26 13:27:45 +1300

    있지

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,43 +5,60 @@ using UnityEngine;
 public class BallController : MonoBehaviour {
 
     //Declare Public variables
-    public float OrbitHeight1 = 2.5f;
-    public float FallStep = 0.1f;
+    public float FallSpeed = 0.01f;
+    public float BounceSpeed = 0.2f;
+    public float[] OrbitHeights;
 
     //Declare Private variables
-    private float CurrentHeight = 0;
     private bool Down = false;
+    private bool Up = false;
+    private bool Clickable = true;
+    private int Lvl;
+    private Vector3 FallVector;
+    private Vector3 UpVector;
 
-	// Use this for initialization
-	void Start () {
-        CurrentHeight = OrbitHeight1;
-        transform.localPosition = new Vector3(0, CurrentHeight, 0);
-	}
+    // Use this for initialization
+    void Start () {
+
+        //Initialise orbit
+        Lvl = 0;
+        transform.localPosition = new Vector3(0, OrbitHeights[Lvl], 0); //Set the initial player position to the height of the first orbit layer
+
+        FallVector = new Vector3(0, FallSpeed, 0);
+        UpVector = new Vector3(0, BounceSpeed, 0);
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if (Input.GetMouseButton(0))
+        
+        //At any time if the position of the ball is higher than the orbit, adjust to the current orbit's height
+        if (transform.localPosition.y > OrbitHeights[Lvl])
         {
-            Down = true;
+            transform.localPosition = new Vector3(0, OrbitHeights[Lvl], 0);
+            Up = false;
         }
 
-        //User clicked, ball speeds down.
+        //We can only click mousebutton when the ball isn't coming down
+        if (Clickable)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Down = true;
+                Up = false;
+                Clickable = false;
+            }
+        }
+
+        //while "Down" ball speeds down.
         if (Down)
         {
-            CurrentHeight -= FallStep;
-            transform.localPosition = new Vector3(0, CurrentHeight, 0);
+            transform.localPosition -= FallVector;
         }
-        else
+
+        //while "Up" the ball speeds up.
+        if (Up)
         {
-            if (CurrentHeight < OrbitHeight1)
-            {
-                CurrentHeight += FallStep;
-                if(CurrentHeight > OrbitHeight1)
-                {
-                    CurrentHeight = OrbitHeight1;
-                }
-                transform.localPosition = new Vector3(0, CurrentHeight, 0);
-            }
+            transform.localPosition += UpVector;
         }
 	}
 
@@ -51,6 +68,15 @@ public class BallController : MonoBehaviour {
         if(collision.tag == "Planet")
         {
             Down = false;
+            Up = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Planet")
+        {
+            Clickable = true;
         }
     }
 }
