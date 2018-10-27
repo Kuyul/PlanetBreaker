68de47b7ddb50ce0f4c12507c3d7da4ab00552a4
Kuyul <peyopeyo92@gmail.com>
2018-10-27 14:43:12 +1300

    Update Assets/Scripts/CameraControl.cs

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -11,6 +11,10 @@ public class CameraControl : MonoBehaviour
     private Camera Cam;
     private float CamOrigSize;
     private GameControl Gc;
+    private int CurrentLevel;
+    private int PreviousLevel;
+    private bool GoingUp = false;
+    private bool LevelReset = false;
 
     // Use this for initialization
     void Start()
@@ -18,19 +22,59 @@ public class CameraControl : MonoBehaviour
         Cam = Camera.main;
         CamOrigSize = Cam.orthographicSize;
         Gc = GameControl.Instance;
+
+        //Initialise Previous level
+        PreviousLevel = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float ballHeight = Ball.localPosition.y;
-        int lvl = Gc.GetOrbitLevel();
         float[] orbitHeights = Gc.GetOrbitHeights();
-        if(lvl > 0)
-        if (ballHeight > orbitHeights[lvl-1])
+        //Get the current ball height
+        float ballHeight = Ball.localPosition.y;
+        //Get the current orbit level
+        CurrentLevel = Gc.GetOrbitLevel();
+
+        //Check if there was an increase in orbit level, if so, set goingup flag to true until it reaches the orbit height
+        if(CurrentLevel > PreviousLevel)
         {
-            float scale = Ball.localPosition.y / orbitHeights[0];
-            Cam.orthographicSize = CamOrigSize * scale;
+            GoingUp = true;
+        }else if (CurrentLevel < PreviousLevel)
+        {
+            LevelReset = true;
         }
+
+        //When the ball is going up up a orbit
+        if (GoingUp)
+        {
+            if (ballHeight > orbitHeights[CurrentLevel - 1])
+            {
+                float scale = Ball.localPosition.y / orbitHeights[0];
+                Cam.orthographicSize = CamOrigSize * scale;
+            }
+
+            //When the ball has reached the orbit height, set goingup bool to false
+            if(ballHeight == orbitHeights[CurrentLevel])
+            {
+                GoingUp = false;
+            }
+        }
+
+        //Zoom in until it reaches the initial scale
+        if (LevelReset)
+        {
+            if(Cam.orthographicSize > CamOrigSize)
+            {
+                Cam.orthographicSize -= 0.1f;
+            }
+            else
+            {
+                LevelReset = false;
+            }
+        }
+
+        //TODO: When the ball falls in orbit
+        PreviousLevel = CurrentLevel;
     }
 }
