a0d6b4f2e460076cced161b4d286ea0a45b79e1f
Kuyul <peyopeyo92@gmail.com>
2018-10-31 12:21:50 +1300

    ㅎㅅㅎ

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+    //Declare public variables
+    public Rigidbody2D Planet;
+
+    public float planetSpinSpeed; //TODO: remove this later as we won't need it
+
+	// Use this for initialization
+	void Start () {
+        GenerateLevel(planetSpinSpeed);
+	}
+
+    private void GenerateLevel(float planetSpin)
+    {
+        Planet.angularVelocity = planetSpin;
+    }
+}
